1bca13dda50443100fc223abf77f50da2b883b56
刘佳怡 <liujiayi@czxy.com>
2018-09-03 16:54:00 +0800

    jj

--- public/index.php
+++ public/index.php
@@ -14,22 +14,29 @@ function autoload($class){
     //注册加载函数
     spl_autoload_register('autoload');
 
-
-  if(isset($_SERVER['PATH_INFO'])){
-
-    $pathInfo = $_SERVER['PATH_INFO'];
-    //根据/转成数组
-    $pathInfo = explode('/',$pathInfo);
+//添加路由：解析URL浏览器上blog/index CLI中就是 blog index
+  if(php_sapi_name()=='cli'){
     
     //得到控制器和方法名
-    $controller = ucfirst($pathInfo[1]).'Controller';
-    $action = $pathInfo[2];
+    $controller = ucfirst($argv[1]).'Controller';
+    $action = $argv[2];
    
   }else{
+    if( isset($_SERVER['PATH_INFO']) )
+    {
+        $pathInfo = $_SERVER['PATH_INFO'];
+        // 根据 / 转成数组
+        $pathInfo = explode('/', $pathInfo);
+
+        // 得到控制器名和方法名 ：
+        $controller = ucfirst($pathInfo[1]) . 'Controller';
+        $action = $pathInfo[2];
+    }else{
     //默认控制器和方法
     $controller = 'IndexController';
     $action = 'Index';
   }
+}
  
 
   $fullController = 'controller\\'.$controller;
